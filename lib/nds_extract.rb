54b79568d92ded608e7772e02d2b266b934c24bf
Dino Kunovac <dinokunovac@gmail.com>
2020-01-01 20:26:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,14 +8,6 @@ def directors_totals(nds)
   
   result = {
   }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
+  
   nil
 end
