870be6dfcad81334916aa90d44abe175833367da
Dino Kunovac <dinokunovac@gmail.com>
2020-01-01 20:28:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,8 +4,21 @@ require 'directors_database'
 def directors_totals(nds)
   director_index = 0
   result = {
-    
+    while director_index < nds.length do
+    director_name = nds[director_index][:name]
+    result[director_name] = 0
+    movie_index = 0
+
+    while movie_index < nds[director_index][:movies].length do
+      result[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+
+    director_index += 1
+  end
+
+  result
   }
   
-  nil
+  
 end
